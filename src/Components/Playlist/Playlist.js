1cf262441e8d2c7fe87d3999538a9d995c6640ca
John Steele <j.steelecg@gmail.com>
2024-08-28 01:38:14 -0400

    add additional attributes to tracklist in playlist file

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,9 @@ function Playlist(props) {
     return (
         <div style={{width: '50%', marginTop: 50}}>
             <input placeholder="Playlist Name..." onChange={handleNameChange} />
-            <Tracklist userAdd={props.playlistTracks} onRemove={props.onRemove} isRemoval='true' />
+            {/* TODO: remove bottom line... only for testing purposes. */}
+            <p>{props.playlistName}</p>
+            <Tracklist playlist={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} isPlaylist={true} />
             <button>Save To Spotify</button>
         </div>
     )
