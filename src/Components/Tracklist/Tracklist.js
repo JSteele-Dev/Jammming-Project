0628d28428dc5bbf0761b442a354c54ad070f07e
John Steele <j.steelecg@gmail.com>
2024-08-28 01:41:21 -0400

    add method to render tracklist depending if it is from the search results or playlist

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Tracks from "../Track/Track";
 
 function Tracklist(props) {
+    const listing = () => {
+        if (props.isPlaylist) {
+            return (
+                props.playlist.map((songs) => (
+                    <Tracks track={songs} key={songs.id} onRemoval={props.onRemove} isRemoval={props.isRemoval} />
+                ))
+            );
+        } else {
+            return (
+                props.userResult?.map((songs) => (
+                    <Tracks track={songs} key={songs.id} onAdd={props.onAdd} isRemoval={props.isRemoval} />
+                ))
+            )
+        }
+    }
+
+    useEffect (() => {
+        console.log(props.isPlaylist);
+    }, [props.isPlaylist])
+
     return (
         <div>
-            {props.userResult?.map((songs) => (
-                <Tracks track={songs} key={songs.id} onAdd={props.onAdd} onRemoval={props.onRemoval} isRemoval={props.isRemoval} />
-            ))}
+            {listing()}
         </div>
     )
 }
