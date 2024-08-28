67b2c247c12a5de1f13e52b36d104dc8bba5df4f
John Steele <j.steelecg@gmail.com>
2024-08-28 01:37:14 -0400

    update tracklist component in searchresults to pass boolean to identify if it is the playlist or search results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,7 +4,7 @@ import Tracklist from "../Tracklist/Tracklist";
 function SearchResults(props) {
     return (
         <div style={{width: '50%', marginTop: 50}}>
-            <Tracklist userResult={props.userResult} isRemoval='false' onAdd={props.onAdd} />
+            <Tracklist userResult={props.userResult} isRemoval={false} onAdd={props.onAdd} isPlaylist={false} />
         </div>
     )
 }
